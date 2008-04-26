dcb64d72e8b79cd41c2d75aeb708c52d5b63062d
Aiolos1127 <Aiolos1127@3e89f612-834b-0410-bb31-dbad55e6f342>
2008-04-26 08:27:31 +0000

    the first init code for simulating memory

---
+++ trunk/cortex/memory.c
@@ -0,0 +1,30 @@
+#include "memory.h"
+static int memory[MEM_SIZE]={0};
+void memory_copy(int target, int destination, int num){
+	int i;
+	for (i =0; i< num; i++){
+		assert(destination+i < MEM_SIZE && target + i < MEM_SIZE);
+		memory[destination+i] = memory[target+i];
+	}
+}
+
+int get_memory(int address){
+	assert(address < MEM_SIZE);
+	return memory[address];
+}
+
+void set_memory(int address, int value){
+	assert(address < MEM_SIZE);
+	memory[address] = value;
+}
+
+void clear_memory(int target, int  num){
+	int i;
+	for ( i =0; i< num; i++){
+		assert( target + i < MEM_SIZE);
+	    memory[target+i]=0;
+	}
+}
+int get_memory_size(){
+	return MEM_SIZE;
+}
