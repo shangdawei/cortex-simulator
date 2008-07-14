cdf35216d7599b5338e7343a0ebfcb409c0d08da
h.t.jacky <h.t.jacky@3e89f612-834b-0410-bb31-dbad55e6f342>
2008-07-14 08:20:32 +0000

    第二类指令完成，进入测试阶段！

--- cortex/helper_procedures.c
+++ cortex/helper_procedures.c
@@ -22,20 +22,43 @@ int ArchVersion()
 	return 2;
 }
 
-bool Bad_Reg(int n){
+bool Bad_Reg(int n)
+{
 //This function performs the check for the register numbers 13 and 15 that are disallowed for many Thumb register specifiers.
 	return (n == 13 || n == 15);
 }
 
-bool InITBlock(){
+bool InITBlock()
+{
 //This function returns TRUE if execution is currently in an IT block and FALSE otherwise.
 }
 
-bool LastInITBlock(){
+bool LastInITBlock()
+{
 //This function returns TRUE if the current instruction is the last instruction in an IT block, and FALSE otherwise.
 }
 
-void LoadWritePC(int value){
+void LoadWritePC(int value)
+{
 //This procedure writes a value to the PC with BX-like interworking behavior for writes by load instructions.
 	set_pc(value);
+}
+bool IntegerZeroDivideTrappingEnabled()
+{
+/*
+This function returns TRUE if the trapping of divisions by zero in the integer division instructions SDIV
+and UDIV is enabled, and FALSE otherwise.
+In the M profile, this is controlled by the DIV_0_TRP(P452) bit in the Configuration Control register. TRUE is
+returned if the bit is 1 and FALSE if it is 0.
+*/
+	return true;
+}
+
+void RaiseIntegerZeroDivide()
+{/*
+This procedure raises the appropriate exception for a division by zero in the integer division instructions
+SDIV and UDIV.
+In the M profile, this is a UsageFault exception.
+*/
+	printf("Usage fault exception: a division by zero in the integer division instructions!\n");
 }
