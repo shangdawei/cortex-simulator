72fa54ca2b2a89c95779864a3891fdddf23b1210
dwill1987 <dwill1987@3e89f612-834b-0410-bb31-dbad55e6f342>
2008-12-02 14:04:53 +0000

    补充实现了IT Blcok指令 顺道修改了几个thumb_misc的几个小错误

--- cortex/main.c
+++ cortex/main.c
@@ -11,6 +11,8 @@ int main(){
 	load_elf("test.elf");
 //--------------//
 	while(1){
+
+
 		unsigned int instruction= get_MemU(get_pc()& 0xfffffffe,2);
 	//	instruction &= 0x0ffff;
 	//	if(state==1){
@@ -22,13 +24,35 @@ int main(){
 							((instruction & 0x0F000)==0x0F000)){
 					instruction=(((get_MemU((get_pc()&0xfffffffe),2)<<16)&0xffff0000) | (get_MemU((get_pc()&0xfffffffe)+2,2)&0x0ffff));
 					set_pc(get_pc()+4);
-					decode(instruction);
+						//deal with the IT instruction
+					if(InITBlock()){   //in IT Block
+						if(ConditionPassed((get_epsr()>>12)&0x0f))
+							decode(instruction);
+
+						if(LastInITBlock())
+							set_epsr(get_epsr()&0xf9ff03ff);
+						else
+							set_epsr((((get_epsr()&0x06001c00)<<1)&0x06001c00)|(get_epsr()&0x0100e000)|((get_epsr()>>16)&0x00000400));
+					}
+					else   //not in IT Block
+						decode(instruction);
 	//				state=1;
 
 				}
 				else{
 					set_pc(get_pc()+4);
-					thumb_decode((short)instruction);
+					if(InITBlock()){   //in IT Block
+						if(ConditionPassed((get_epsr()>>12)&0x0f))
+							decode(instruction);
+
+						if(LastInITBlock())
+							set_epsr(get_epsr()&0xf9ff03ff);
+						else
+							set_epsr((((get_epsr()&0x06001c00)<<1)&0x06001c00)|(get_epsr()&0x0100e000));
+					}
+
+					else   //not in IT Block
+						thumb_decode(instruction);
 					set_pc(get_pc()-2);
 //					if(state==1)
 //						set_pc(get_pc()+2);
