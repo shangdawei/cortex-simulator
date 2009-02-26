361080bb4feedb407775365a9c070b4348c3e02c
yihengw <yihengw@3e89f612-834b-0410-bb31-dbad55e6f342>
2009-02-26 12:54:15 +0000

    This time I add the vbus (communicate function between simulator and other process) into the project. vbus-dev is the development files of vbus

--- vbus/vbus_procedures.c
+++ vbus/vbus_procedures.c
@@ -2,26 +2,17 @@
 
 bool loadconfig(char* filename, vbus_descriptor* des)
 {
-	des->INT_number = 5;
-	des->INTsize = 256;
-	des->INTstart = 2;
-	des->event_addr = 0;
 	des->poolsize = 4096;
-	des->busname[0] = 'v';
-	des->busname[1] = 'b';
-	des->busname[2] = 'u';
-	des->busname[3] = 's';
-	des->busname[4] = '\0';
 
 	return TRUE;
 }
 
 bool init_vbus(vbus_descriptor* des)
 {
-	return win32_build_bus(des->busname,des->poolsize);
+	return win32_build_bus("vbus",des->poolsize);
 }
 
-bool release_vbus()
+void release_vbus()
 {
-	return win32_release_bus();
+	win32_release_bus();
 }
